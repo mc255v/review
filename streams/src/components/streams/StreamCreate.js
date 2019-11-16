c143ce0e870bdb9c705c13479ce94fb2d215df77
Matthew Cook <mc255v@gmail.com>
2019-11-16 19:14:40 +0900

    finished action creators

--- streams/src/components/streams/StreamCreate.js
+++ streams/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions'
 
 class StreamCreate extends React.Component{
   renderError({ error, touched}) {
@@ -17,14 +19,14 @@ class StreamCreate extends React.Component{
     return (
       <div className={className}>
         <label>{label}</label>
-        <input {...input} />
+        <input {...input} autoComplete="off"/>
         {this.renderError(meta)}
       </div>
     );
   }
 
-  onSubmit(formValues) {
-    
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues);
   }
 
   render(){
@@ -45,7 +47,9 @@ const validate = (formValues) => {
   return errors;
 }
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: 'streamCreate',
   validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
