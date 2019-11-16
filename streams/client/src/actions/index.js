351b8b8eec7f67926db6d576b2ff85aecf4e90fc
Matthew Cook <mc255v@gmail.com>
2019-11-16 15:03:44 +0900

    hooked up redux to project

---
+++ streams/client/src/actions/index.js
@@ -0,0 +1,9 @@
+import { SIGN_IN, SIGN_OUT } from './types'
+
+export const signIn = (userId) => {
+  return { type: SIGN_IN, userId }
+}
+
+export const signOut = () => {
+  return { type: SIGN_OUT }
+}
