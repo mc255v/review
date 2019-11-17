88534c8a643e2029acc2af7e4f0e9f6a381a08d7
Matthew Cook <mc255v@gmail.com>
2019-11-17 09:59:40 +0900

    refactored to use router with custom history

--- streams/src/components/App.js
+++ streams/src/components/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import StreamList from './streams/StreamList';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamShow from './streams/StreamShow';
 import Header from './Header';
+import history from '../history';
 
 
 const App = () => {
   return(
-    <div>
-      <BrowserRouter>
+    <div className="ui container">
+      <Router history={history}>
         <Header />
         <Route exact path="/" component={StreamList} />
         <Route exact path="/streams/new" component={StreamCreate} />
-        <Route exact path="/streams/edit" component={StreamEdit} />
-        <Route exact path="/streams/delete" component={StreamDelete} />
+        <Route exact path="/streams/edit/:id" component={StreamEdit} />
+        <Route exact path="/streams/delete/:id" component={StreamDelete} />
         <Route exact path="/streams/show" component={StreamShow} />
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
