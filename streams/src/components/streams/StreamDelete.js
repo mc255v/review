2abbe432069b88167a169c2b9117c01af4934d12
Matthew Cook <mc255v@gmail.com>
2019-11-17 10:02:35 +0900

    basic structure for modal when stream delete

--- streams/src/components/streams/StreamDelete.js
+++ streams/src/components/streams/StreamDelete.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import Modal from '../Modal';
+import history from '../../history';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamDelete = () => {
+class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+  renderActions() {
+    return (
+      <>
+        <button className="ui button negative">Delete</button>
+        <button className="ui button">Cancel</button>
+      </>
+    );
+  }
+
+  renderContent() {
+    if (!this.props.stream) {
+      return 'Are you sure you want to delete this stream?'
+    }
+    return `Are you sure you want to delete the stream "${this.props.stream.title}"?`
+  }
+
+  render() {
   return (
-    <div></div>
+      <Modal 
+        title="Delete Stream"
+        content={this.renderContent()}
+        actions={this.renderActions()}
+        onDismiss={() => history.push('/')}
+      />
   );
+  }
 }
 
-export default StreamDelete;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] }
+}
+
+export default connect(mapStateToProps, { fetchStream })(StreamDelete);
